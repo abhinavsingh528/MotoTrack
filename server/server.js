607d46f386d73eeb2141897189dafbbcc1c35c2d
abhinavsingh528 <abhinavanimation786@gmail.com>
2026-03-24 16:23:36 +0530

    Add packages

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("MotoTrack API is running 🚀");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
